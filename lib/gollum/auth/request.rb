0863b114eb5a39998137e29b4451c8394cf00955
ViChyavIn <nikita.vyach.ivanov@gmail.com>
2020-11-24 16:09:58 +0500

    Fix bug in base_path setting

--- lib/gollum/auth/request.rb
+++ lib/gollum/auth/request.rb
@@ -2,15 +2,17 @@ require 'json'
 
 module Gollum::Auth
   class Request < Rack::Request
+
     WRITE_PATH_RE = %r{
       ^/
       (gollum/)? # This path prefix was introduced in Gollum 5
       (create/|edit/|delete/|rename/|revert/|uploadFile$|upload_file$)
     }x
 
-    def initialize(*)
-      super
+    def initialize(env, base_path='')
+      super(env)
       @data_string = @env['rack.input'].read
+      @base_path = base_path
     end
 
     def requires_authentication?(allow_unauthenticated_readonly)
@@ -36,11 +38,18 @@ module Gollum::Auth
       JSON.parse(cookie_string)
     end
 
+    def wiki_path
+      a = path_info.dup
+      a.slice! @base_path
+      return a
+    end
+
     private
 
     # Returns true if path is a write path that would change the wiki.
     def is_write_path?
-      !!(path_info =~ WRITE_PATH_RE)
+      !!(wiki_path =~ WRITE_PATH_RE)
     end
+
   end
 end
